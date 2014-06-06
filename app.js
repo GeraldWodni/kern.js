a2627ea4b812aebb21a314b2101c7637096cbd45
Gerald Wodni <gerald.wodni@gmail.com>
2014-06-06 03:12:07 +0200

    multi-process forking added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var kern = require("./kern");
 
 var app = kern();
+app.debug("App Running in Worker #" + app.status.workerId );
 
 module.exports = app;
 
--- kern.js
+++ kern.js
@@ -1,25 +1,53 @@
+var cluster = require("cluster");
+//var hub = require("clusterhub");
+var os = require("os");
 var http = require("http");
-var debug = require("debug")("kern");
 var _ = require("underscore");
 
 /* default value for kern instances */
 var defaults = {
     port: 3000
+    // processCount: specify the number of worker-processes to create
 };
 
 /* main export */
 var Kern = function( opts ) {
     
     opts = _.extend( defaults, opts );
+    var status = {
+        workerId: cluster.isMaster ? 0 : cluster.worker.id
+    };
+
+    function debug() {
+        console.log.apply( this, arguments );
+    };
+
+    function worker() {
+        http.createServer( function( req, res ) {
+            res.writeHead( 200, { 'Content-Type': 'text/plain' } );
+            res.end( "Hello, here is kern.js [" + status.workerId + "]" );
+            debug( "GET, " + status.workerId );
+        }).listen( opts.port );
+    }
 
     return {
+        debug: debug,
+        worker: worker,
+        status: status,
         run: function() {
-            debug( "Listening on Port " + opts.port );
 
-            http.createServer( function( req, res ) {
-                res.writeHead( 200, { 'Content-Type': 'text/plain' } );
-                res.end( "Hello, here is kern.js" );
-            }).listen( opts.port );
+            if( cluster.isMaster ) {
+                /* form workers */
+                var processCount = opts.processCount || os.cpus().length;
+                debug( "Master, starting " + processCount + " workers" );
+                for( var i = 0; i < processCount; i++ )
+                    cluster.fork();
+
+            } else {
+                /* worker */
+                debug( "Worker on Port " + opts.port + ", id:" + status.workerId );
+                worker();
+            }
         }
     };
 };
