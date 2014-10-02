6b312d57401caa71b2ceae159fb95c31786dc076
Gerald Wodni <gerald.wodni@gmail.com>
2014-10-03 01:37:21 +0200

    navigation GET /edit started

--- websites/default/navigation.js
+++ websites/default/navigation.js
@@ -1,9 +1,10 @@
 // navigation administration module
 // (c)copyright 2014 by Gerald Wodni <gerald.wodni@gmail.com>
 
-var bcrypt  = require("bcrypt-nodejs");
-var colors  = require("colors");
-var util    = require("util");
+var bcrypt      = require("bcrypt-nodejs");
+var colors      = require("colors");
+var util        = require("util");
+var querystring = require("querystring");
 
 
 module.exports = {
@@ -74,23 +75,43 @@ module.exports = {
 
 
         k.router.get( "/all",   function( req, res, next ) { 
-		k.rdb.navigation.readAll( k.website, function( err, data ) {
-			console.log( err );
-			if( err )
-				return next( err );
-			res.send( data );
-		});
-	});
+                k.rdb.navigation.readAll( k.website, function( err, data ) {
+                        console.log( err );
+                        if( err )
+                                return next( err );
+                        res.send( data );
+                });
+        });
 
         k.router.get( "/asd",   function( req, res ) { k.renderJade( req, res, "admin/info" ); } );
-        k.router.get( "/",      function( req, res ) {
+
+        function renderAll( req, res, values ) {
             k.rdb.navigation.readAll( req.website, function( err, items ) {
                 if( err )
                     next( err );
+                    
+                items.forEach( function( item ) {
+                    item.escapedLink = encodeURIComponent( item.link );
+                });
             
                 console.log( "RENDER".red.bold );
-                k.renderJade( req, res, "admin/navigation", { messages: req.messages, items: items } );
+                /* TODO: generic non-JS version */
+                k.renderJade( req, res, "admin/navigation", { messages: req.messages, items: items, values: values } );
             });
+        }
+
+        k.router.get( "/edit/:link?", function( req, res ) {
+            console.log( "ASD:", req.requestData.escapedLink( 'link' ) );
+            k.rdb.navigation.read(req.website, req.requestData.escapedLink( 'link' ), function( err, data ) {
+                if( err )
+                    next( err );
+
+                renderAll( req, res, data );
+            });
+        });
+
+        k.router.get( "/", function( req, res ) {
+            renderAll( req, res );
         });
     }
 };
