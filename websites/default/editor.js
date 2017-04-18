4d7744fe0c81c7d9c329085dd8651084d6c2ea1b
Gerald Wodni <gerald.wodni@gmail.com>
2017-04-19 00:33:31 +0200

    editor can now load file-content

--- websites/default/editor.js
+++ websites/default/editor.js
@@ -1,10 +1,6 @@
 // editor administration module
 // (c)copyright 2017 by Gerald Wodni <gerald.wodni@gmail.com>
 
-var bcrypt      = require("bcrypt-nodejs");
-var colors      = require("colors");
-var util        = require("util");
-var querystring = require("querystring");
 var _           = require("underscore");
 
 module.exports = {
@@ -12,24 +8,37 @@ module.exports = {
 
         var hierarchyFilters = {
             dirShowFilters:  [ /views/g, /css/g, /js/g, /^$|^\/admin\/editor\/edit$/g ],
-            fileShowFilters: [ /.*\.css$/g, /.*\.jade$/g, /.*\.js$/g, /.*\.jsxon$/, /.*\.less$/g, /.*\.md$/g ],
+            fileShowFilters: [ /.*\.css$/g, /.*\.jade$/g, /.*\.js$/g, /.*\.json$/, /.*\.less$/g, /.*\.md$/g ],
             lockWebsite: true
         };
+
+        function renderAll( req, res, values ) {
+            k.hierarchy.readHierarchyTree( req.kern.website, ".", _.extend( {}, hierarchyFilters, {
+                prefix: "/admin/editor/edit"
+            }),
+            function( err, tree ) {
+                k.jade.render( req, res, "admin/editor", _.extend( { tree: tree }, values ) );
+            });
+        }
 
         k.router.get("/edit/*", function( req, res ) {
             var filename = req.params[0];
             if( !k.hierarchy.checkFilters( req.kern.website, filename, hierarchyFilters ) )
                 return k.httpStatus( req, res, 403 );
-            k.hierarchy.createReadStream( req.kern.website, filename ).pipe( res );
+
+            /* read file into string */
+            var rs = k.hierarchy.createReadStream( req.kern.website, filename );
+            var chunks = [];
+            rs.on("data", function( data ) {
+                chunks.push( data );
+            });
+            rs.on("end", function() {
+                renderAll( req, res, { content: Buffer.concat( chunks ).toString() } );
+            });
         });
 
         k.router.get( "/", function( req, res ) {
-            k.hierarchy.readHierarchyTree( req.kern.website, ".", _.extend( {}, hierarchyFilters, {
-                prefix: "/admin/editor/edit"
-            }),
-            function( err, tree ) {
-                k.jade.render( req, res, "admin/editor", { tree: tree } );
-            });
+            renderAll( req, res );
         });
     }
 };
