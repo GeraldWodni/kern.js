35e529d8af9e3a05ef317b21bc78822cf11108a2
Gerald Wodni <gerald.wodni@gmail.com>
2022-01-18 19:03:31 +0100

    http state 423: Locked added

--- httpStati.js
+++ httpStati.js
@@ -6,6 +6,7 @@ module.exports = {
     403: { title: "Forbidden", text: "You are not entitled to access the specified resource." },
     404: { title: "Not Found", text: "The specified resource could not be found." },
     422: { title: "Unprocessable Entity", text: "The request contains semantic errors." },
+    423: { title: "Locked", text: "The resource that is being accessed is locked." },
     501: { title: "Not Implemented", text: "The request cannot be fulfilled." },
     502: { title: "Bad Gateway", text: "Invalid response received from upstream server." },
     503: { title: "Service Unavailable", text: "The server is currently unavailable, or you have reached an unconfigured host." }
