93f35a2e0c51ebac0724f4a12caa787792921611
Gerald Wodni <gerald.wodni@gmail.com>
2022-09-07 19:17:29 +0200

    env-single.sh helper script added for simple debugging of stand alone instances

---
+++ env-single.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <website>"
+    exit 1
+fi
+
+WEBSITE=$1
+LOCALE=de-AT
+
+KERN_STATIC_LOCALE=$LOCALE
+export KERN_STATIC_LOCALE
+KERN_STATIC_HOST=$WEBSITE
+export KERN_STATIC_HOST
+KERN_LOAD_ONLY_HOSTS=$WEBSITE
+export KERN_LOAD_ONLY_HOSTS
+KERN_AUTO_LOAD=false
+export KERN_AUTO_LOAD
+
+npm start
+
